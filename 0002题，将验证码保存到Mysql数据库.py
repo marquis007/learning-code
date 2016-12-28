63e28dff6fdaa36c98629b8ff0473ec61e8956c6
marquis007 <xsjl920@sina.com>
2016-12-28 11:43:52 +0800

    Update 0002题，将验证码保存到Mysql数据库.py

--- 0002题，将验证码保存到Mysql数据库.py
+++ 0002题，将验证码保存到Mysql数据库.py
@@ -12,7 +12,7 @@ def coupondata(s, num):
         choicecode = string.ascii_letters + string.digits
         code = [random.choice(choicecode) for i in range(int(s))]
         code = "".join(code)
-        sql = "insert into test(code) values(%s)"
+        sql = "insert into test(code) values(%s)"     ###将生成的code插入到数据表中的sql语句
         try:
             cursor.execute(sql,[code])
         except BaseException as e:
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="127.0.0.1",
                            port=3306,
                            user="root",
-                           password="cs1030",
-                           db="test",
+                           password="your password",
+                           db="db name",
                            charset="utf8"
                            )
 
