fb044d8e9cd30ead32c4b912afd029ebaf2a5a5f
marquis007 <xsjl920@sina.com>
2016-12-29 20:46:24 +0800

    Update 0002题，将验证码保存到Mysql数据库.py
    首先利用sqlyog创建数据库和表格，这里把数据表命名为coupon

--- 0002题，将验证码保存到Mysql数据库.py
+++ 0002题，将验证码保存到Mysql数据库.py
@@ -1,40 +1,65 @@
-# -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*- 
 import MySQLdb
-import random
-import string
 import sys
-
-
-
-def coupondata(s, num):
-    cursor = conn.cursor()
-    for i in range(num):
-        choicecode = string.ascii_letters + string.digits
-        code = [random.choice(choicecode) for i in range(int(s))]
-        code = "".join(code)
-        sql = "insert into test(code) values(%s)"     ###将生成的code插入到数据表中的sql语句
+import string
+import random
+class Coupon(object):
+    def __init__(self,conn):
+        self.conn = conn
+    
+    def coupon_digit(self,digit,num):
+        cursor = conn.cursor()
+        for i in range(int(num)):
+            choicecode = string.ascii_letters + string.digits
+            code = [random.choice(choicecode) for s in range(int(digit))]
+            code = "".join(code)
+            sql = "insert into coupondata(coupon) values(%s)"
+            try:
+                cursor.execute(sql,[code])
+            except Exception as e:
+                print "插入验证码失败 " + str(e)
+                cursor.close()
+#     def coupon_num(self,num):
+#         cursor = conn.cursor()
+#         sql = "insert into coupondata(code) values(%s)"
+#         for i in range(int(num)):
+#             try:
+#                 cursor.execute(sql,[i])
+#             except Exception as e:
+#                 print "插入序号失败 " + str(e)
+#                 break
+        
+    def transfer(self,digit,num):
         try:
-            cursor.execute(sql,[code])
-        except BaseException as e:
-            print(e)
-            break
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print '完成'
-
-
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="127.0.0.1",
-                           port=3306,
-                           user="root",
-                           password="your password",
-                           db="db name",
-                           charset="utf8"
-                           )
-
-    print coupondata(13,200)
-
+            self.coupon_digit(digit,num)
+#             self.coupon_num(num)
+            self.conn.commit()
+        except Exception as e:
+            print "插入数据失败" + str(e)
+            self.conn.commit()
+        finally:
+            self.conn.rollback()
+           
+    
+if __name__=="__main__":
+    conn = MySQLdb.connect(host = "127.0.0.1",
+                       port = 3306,
+                       user = "root",
+                       password = "your passwd",
+                       db = "your db",
+                       charset = "utf8"
+                       )
+    digit = sys.argv[1]
+    num = sys.argv[2]
+    coupondata = Coupon(conn)
+    try:
+        coupondata.transfer(digit,num)
+    except Exception as e:
+        print "出现问题" + str(e)
+    finally:
+        conn.close() 
+    
+    
 
 
 
